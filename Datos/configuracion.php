cdff4e51663550eb67e144a13a510bff9d18f96a
oterrones <lterronesortiz@gmail.com>
2015-12-16 15:12:27 -0500

    CONEXION

--- Datos/configuracion.php
+++ Datos/configuracion.php
@@ -2,6 +2,6 @@
 
 define("SERVIDOR_BD", "localhost");
 define("USUARIO_BD", "postgres");
-define("CLAVE_BD", "llenque");
-define("PUERTO_BD", "5432");
+define("CLAVE_BD", "12345");
+define("PUERTO_BD", "5433");
 define("NOMBRE_BD", "SistemaPE");
