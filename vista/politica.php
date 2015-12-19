9ae82d4e7539317c136201378db693c088e4044f
oterrones <lterronesortiz@gmail.com>
2015-12-18 19:20:49 -0500

    politica

--- vista/politica.php
+++ vista/politica.php
@@ -80,7 +80,7 @@
                     <div class="modal-content">
                       <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="titulomodal">Editar eje</h4>
+                        <h4 class="modal-title" id="titulomodal">Editar Politica</h4>
                       </div>
                       <div class="modal-body">
                           <div class="row">
