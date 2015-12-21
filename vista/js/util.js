3c873ce6c401780b10e4ec7e88675938f3097327
oterrones <lterronesortiz@gmail.com>
2015-12-21 11:10:38 -0500

    aa

--- vista/js/util.js
+++ vista/js/util.js
@@ -13,3 +13,48 @@ function validarLetras(evento){
     }    
     return false;
 }
+
+function soloLetras(e){
+       var key = e.keyCode || e.which;
+       var tecla = String.fromCharCode(key).toLowerCase();
+       var letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+       var especiales = "8-37-39-46";
+
+       var tecla_especial = false
+       for(var i in especiales){
+            if(key == especiales[i]){
+                tecla_especial = true;
+                break;
+            }
+        }
+
+        if(letras.indexOf(tecla)==-1 && !tecla_especial){
+            return false;
+        }
+    }
+function soloNumeros(e) {
+    var key = e.keyCode || e.which;
+    var tecla = String.fromCharCode(key).toLowerCase();
+    var letras = "0123456789";
+    var especiales = [8, 37, 39, 46];
+
+    var tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial)
+        return false;
+}
+
+
+$(document).on("keypress", "#txtEmpruc", function(){
+    if($("#txtEmpruc").val().length < 11){
+        return true;
+    }else{
+        return false;
+    }
+});
