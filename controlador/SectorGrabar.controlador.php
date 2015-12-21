6abca0a31df705c067b059d0f91f91a8b7899420
danko <dankojared@gmail.com>
2015-12-21 12:15:28 -0500

    SECTOR LISTO

--- controlador/SectorGrabar.controlador.php
+++ controlador/SectorGrabar.controlador.php
@@ -1,11 +1,9 @@
 <?php
-	$sector             = $_POST["txtnombre"];
+	$sector = $_POST["txtsector"];
         
 	require_once '../negocio/TipoSector.class.php';
 	$objTipoSector = new TipoSector();
-        
 	$objTipoSector->setNombre($sector);
-        
 	$resultado=$objTipoSector->agregar();
 	echo json_encode($resultado);
 ?>
